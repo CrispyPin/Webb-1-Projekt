92ed29ccf6494cc9313b71f3eebeb76f1e329077
CrispyPin <CrispyPin@users.noreply.github.com>
2020-03-05 10:46:32 +0100

    fractal tree done

--- fractal-tree.js
+++ fractal-tree.js
@@ -7,13 +7,23 @@ canvas.height = 512;
 let ctx = canvas.getContext("2d");
 
 let globalAngle = Math.PI*0.1;
-let mod = 0.8;
+let mod = 0.75;
+let iterations = 11;
+
+let palette = [
+    "#440",
+    "#480",
+    "#4a0",
+    "#0c0",
+    "#0e4",
+    "#0f4",
+];
 
 function vector(l, angle) {
     let x = Math.cos(angle) * l;
     let y = Math.sin(angle) * l;
     if (angle > Math.PI / 2) {
-        x = -x;
+        x = x;
     }
     return {
         x: x,
@@ -22,33 +32,31 @@ function vector(l, angle) {
 }
 
 function tree(x, y, length, i, angle) {
-    if (i==0) {
-        return;
-    }
-    let right = vector(length * 0.5, angle + globalAngle);
-    let left = vector(length * 0.5, angle - globalAngle);
-    ctx.lineWidth = Math.min(i, 3);
+    if (i==0) { return; }
+
+    let dir = vector(length * mod, angle + globalAngle);
+
     ctx.beginPath();
     ctx.moveTo(x, y);
-    ctx.lineTo(x + left.x, y - left.y);
-    ctx.moveTo(x, y);
-    ctx.lineTo(x + right.x, y - right.y);
+    ctx.lineWidth = Math.min(i, 3);
+    ctx.strokeStyle = palette[iterations-i];
+    ctx.lineTo(x + dir.x, y - dir.y);
     ctx.stroke();
-    tree(x + left.x, y - left.y, length*mod, i-1, angle + globalAngle);
-    tree(x + right.x, y - right.y, length*mod, i-1, angle - globalAngle);
+
+    tree(x + dir.x, y - dir.y, length*mod, i-1, angle - globalAngle);
+    tree(x + dir.x, y - dir.y, length*mod, i-1, angle + globalAngle);
 }
 
-function render(angle) {
-    globalAngle = angle;
+function render() {
     ctx.fillStyle = "#000";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = "#b0b";
-    tree(256, 256, 128, 8, Math.PI/2);
+    tree(256, 512, 128, iterations, Math.PI/2 - globalAngle);
 }
 
 function mouseMove(event) {
-    mod = event.clientY / 1024 + 0.5;
-    render(event.clientX * Math.PI / 256 + Math.PI);
+    //mod = event.clientY / 1024 + 0.5;
+    globalAngle = event.clientX * Math.PI / 256 + Math.PI;
+    render();
 }
 
 canvas.addEventListener("mousemove", mouseMove);
